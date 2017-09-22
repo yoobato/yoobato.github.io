f0d60fda24da2120cc042e825d13aca11c891805
Daeyeol Ryu <dy@yoobato.com>
2017-09-22 19:44:57 +0900

    add gulp for using sass

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+const src_path = {
+  css: 'assets/sass'
+};
+
+const dist_path = {
+  css: 'assets/css'
+};
+
+gulp.task('sass', function() {
+  return gulp.src([src_path.css + '/**/*.scss', src_path.css + '/**/*.sass'])
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest(dist_path.css));
+});
+
+gulp.task('default', ['sass']);
+
+gulp.task('watch', function() {
+  gulp.watch([src_path.css + '/**/*.scss', src_path.css + '/**/*.sass'], ['sass']);
+});
